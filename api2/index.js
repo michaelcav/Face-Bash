385a5472203b7fe9f9d209b7be3fe3e4ec9a18ea
Michael Carvalho <michaeldougmk@gmail.com>
2023-09-07 22:34:07 -0300

    commit

--- api2/index.js
+++ api2/index.js
@@ -5,11 +5,16 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 //middleware
+app.use((req, res, next) => {
+  req.header("Acess-Control-Allow-Credentials", "true");
+  next();
+})
 app.use(express.json());
-app.use(cors({
-  origin: 'http://127.0.0.1:5173', 
-  credentials: true, 
-}));
+app.use(
+  cors({
+    origin: 'http://127.0.0.1:5173',
+  })
+);
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
