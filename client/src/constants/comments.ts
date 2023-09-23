eedfb5aa4c7ddd2a340cae8b34215b1105304376
Michael Carvalho <michaeldougmk@gmail.com>
2023-09-23 04:52:17 -0300

    commit

--- client/src/constants/comments.ts
+++ client/src/constants/comments.ts
@@ -14,7 +14,7 @@ export const comments: Comment[] = [
   {
     id: 1,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam",
-    name: "John Doe",
+    name: "John Doeee",
     userId: 1,
     profilePicture:
       "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
