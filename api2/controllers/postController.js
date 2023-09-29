e535aedc730fc554b1f25eb9ce28abfcf0155d0e
Michael Carvalho <michaeldougmk@gmail.com>
2023-09-29 19:13:48 -0300

    commit

--- api2/controllers/postController.js
+++ api2/controllers/postController.js
@@ -1,19 +1,26 @@
-import { userInfo } from 'os';
 import {db} from '../connect.js'
 import jwt from "jsonwebtoken";
 
-export const getPosts = (req, res)  => {
+export const getPosts = (req, res) => {
+  const userId = req.query.userId;
   const token = req.cookies.accessToken;
-  if(!token) return res.status(401).json("Not logged in")
+  if (!token) return res.status(401).json("Not logged in!");
 
   jwt.verify(token, "secretkey", (err, userInfo) => {
-    if(err) return res.status(403).json("Token is not valid")
-  })
+    if (err) return res.status(403).json("Token is not valid!");
 
-  const q  = `SELECT p.*, u.id as userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationship AS r ON (p.userId = r.followedUserId) WHERE r.followedUserId= ? OR p.userId =?`;
+    console.log(userId);
 
-  db.query(q, [userInfo.id], (err, data ) => {
+    const q =
+      userId !== "undefined"
+        ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
+        : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+    LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
+    ORDER BY p.createdAt DESC`;
+
+  db.query(q, [userInfo.id, userInfo.id], (err, data ) => {
     if (err) return res.status(500).json(err);
     return res.status(200).json(data);
+   })
   });
 }
