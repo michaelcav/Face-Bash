6641101a7f04ef80be9517f77bbe7a19bb39ac5a
Michael Carvalho <michaeldougmk@gmail.com>
2023-09-30 10:26:54 -0300

    commit

--- api2/index.js
+++ api2/index.js
@@ -3,7 +3,7 @@ const app = express();
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 // import userRoutes from "./routes/users.js";
-// import commentRoutes from "./routes/comments.js";
+import commentRoutes from "./routes/comments.js";
 // import likeRoutes from "./routes/likes.js";
 // import relationshipRoutes from "./routes/relationships.js";
 import multer from "multer";
@@ -42,7 +42,7 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
-// app.use("/api/comments", commentRoutes);
+app.use("/api/comments", commentRoutes);
 // app.use("/api/likes", likeRoutes);
 // app.use("/api/relationships", relationshipRoutes);
 
