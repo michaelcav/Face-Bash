1596365b755b3d181b9e1f6c655eba0783ca1adc
Michael Carvalho <michaeldougmk@gmail.com>
2023-10-03 13:14:31 -0300

    commit

--- cli-lama/src/components/update/Update.jsx
+++ cli-lama/src/components/update/Update.jsx
@@ -1,12 +1,27 @@
 import  "./update.scss"
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { makeRequest } from "../../axios";
+
 
 export default function Update({setOpenUpdate}) {
+const [cover, setCover] = useState(null);
+const [profile, setProfile] = useState(null);
   const [texts, setTexts] = useState({
     name: "",
     city: "",
     civilStatus: "",
   })
+
+  const upload = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await makeRequest.post("/upload", formData);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleChange = (e) => {
    setTexts((prev) => ({...prev, [e.target.name]: [e.target.value]}))
@@ -20,7 +35,7 @@ export default function Update({setOpenUpdate}) {
         <input type="text" name="name" onChange={handleChange} />
         <input type="text" name="city" onChange={handleChange} />
         <input type="text" name="civilStatus" onChange={handleChange} />
-        {/* <input type="text" name="name" onChange={handleChange} /> */}
+        <button>Update</button>
       </form>
       <button onClick={()=>setOpenUpdate(false)}>X</button>
     </div>
