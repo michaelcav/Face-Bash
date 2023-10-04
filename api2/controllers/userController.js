996bb6f4dc64a603bc048b877e84d1bfd868f0d6
Michael Carvalho <michaeldougmk@gmail.com>
2023-10-03 21:35:19 -0300

    commit

--- api2/controllers/userController.js
+++ api2/controllers/userController.js
@@ -12,3 +12,32 @@ const q = "SELECT * FROM users WHERE id=?";
     return res.json(info);
   });
 };
+
+export const updateUser = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "UPDATE users SET `name`=?,`city`=?,`civilStatus`=?,`profilePic`=?,`coverPic`=? WHERE id=? ";
+
+    db.query(
+      q,
+      [
+        req.body.name,
+        req.body.city,
+        req.body.civilStatus,
+        req.body.coverPic,
+        req.body.profilePic,
+        userInfo.id,
+      ],
+      (err, data) => {
+        if (err) res.status(500).json(err);
+        if (data.affectedRows > 0) return res.json("Updated!");
+        return res.status(403).json("You can update only your post!");
+      }
+    );
+  });
+};
--- api2/routes/users.js
+++ api2/routes/users.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getUser
-  //  updateUser
+import { getUser,
+   updateUser
   } from "../controllers/userController.js";
 
 const router = express.Router()
 
 router.get("/find/:userId", getUser);
-// router.put("/", updateUser)
+router.put("/", updateUser)
 
 
 export default router
