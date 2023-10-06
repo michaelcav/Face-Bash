9fec585bb4d93c5674a9db45b49b6a087935431b
Michael Carvalho <michaeldougmk@gmail.com>
2023-10-06 20:21:18 -0300

    commit

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../cli-lama/public/upload/");
+    cb(null, "../client/public/upload/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
