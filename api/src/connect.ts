f5d6f138ed61d47043141a7eb1c727dc0bb46fb0
Michael Carvalho <michaeldougmk@gmail.com>
2023-10-06 10:44:45 -0300

    Delete api directory

--- api/src/connect.ts
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2";
-
-export const db = mysql.createConnection({
-  host: "localhost", 
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "social"
-});
--- api/src/interfaces/registerUser.ts
+++
@@ -1,6 +0,0 @@
-export interface RegisterUserType {
-   usernames: string,
-   email: string
-   password: string,
-   name: string
-}
