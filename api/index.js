0e64c5d99c7cd86de697e73d59800140d8ab22e4
Michael Carvalho <michaeldougmk@gmail.com>
2023-10-09 23:24:15 -0300

    commit

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://127.0.0.1:5173",
-    origin: "http://localhost:3000 ",
+    origin: "http://localhost:3000",
   })
 );
 app.use(cookieParser());
